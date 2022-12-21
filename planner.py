afafbbcc77d1ed80d3f4f3bd57044f1ab2631532
zren96 <allen.ren@princeton.edu>
2022-12-21 17:57:55 -0500

    new trajectory

--- planner.py
+++ planner.py
@@ -61,7 +61,8 @@ float_obj = trimesh.load(float_obj_path)
 # target_obj.show()
 
 # Initial pose of the target and float objects - x,y,z,r,p,y - Euler extrinsic XYZ convention
-float_obj_X_init_array = [0.01895152, -0.01185687, -0.17970488, 0, 0.08599904, 1.3]  # from banana_pass1.npz
+float_obj_X_init_array = [0.01895152, -0.01185687, -0.17970488, 0, 0.08599904, -1.3] # from banana_pass1.npz
+# float_obj_X_init_array = [0.01895152, -0.01185687, -0.17970488, -3.05750797, 0.08599904, -1.99028331]
 # float_obj_X_init_array = [0.05, 0.0, 0-0.2+0.023, 0.0, 0.0, 0.0]  # account for 20cm offset in z when loading objects in tcdm
 target_obj_X_array = [-0.02, -0.165, 0-0.2+0.04, 0.0, 0.0, 0.0]
 float_obj_X_init = get_transform(float_obj_X_init_array)
